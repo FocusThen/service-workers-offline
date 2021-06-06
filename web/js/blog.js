ce78864ce5d43376c6b579b3c89bfe5c04202289
M. Mücahit TEZCAN <mmucahit.tezcan@gmail.com>
2021-06-06 22:56:44 +0300

    offonline check do

--- web/js/blog.js
+++ web/js/blog.js
@@ -1,16 +1,34 @@
-(function Blog(){
-	"use strict";
+;(function Blog() {
+  'use strict'
 
-	var offlineIcon;
-	var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "");
+  var offlineIcon
+  var isOnline = 'onLine' in navigator ? navigator.onLine : true
+  var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || '')
+  var usingSW = 'serviceWorker' in navigator
+  var swRegistration
+  var svcwoker
 
-	document.addEventListener("DOMContentLoaded",ready,false);
+  document.addEventListener('DOMContentLoaded', ready, false)
+  initServiceWorker().catch(console.error)
 
+  // **********************************
 
-	// **********************************
+  function ready() {
+    offlineIcon = document.getElementById('connectivity-status')
 
-	function ready() {
-		offlineIcon = document.getElementById("connectivity-status");
-	}
+    if (!isOnline) {
+      offlineIcon.classList.remove('hidden')
+    }
 
-})();
+    window.addEventListener('online', function online() {
+      offlineIcon.classList.add('hidden')
+      isOnline = true
+    })
+
+    window.addEventListener('offline', function offline() {
+      offlineIcon.classList.remove('hidden')
+      console.log(offlineIcon)
+      isOnline = false
+    })
+  }
+})()
